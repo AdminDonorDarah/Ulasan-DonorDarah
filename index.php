1d60a1f33a9332aaec85fc4f2649777f7ca83890
AdminDonorDarah <niiteam80@gmail.com>
2024-09-17 22:03:32 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v20.0" nonce="KOCp17H8"></script>
+<div class="fb-comments" data-href="https://admindonordarah.github.io/Ulasan-DonorDarah/" data-width="" data-numposts="1"></div>
+</body>
+</html>
